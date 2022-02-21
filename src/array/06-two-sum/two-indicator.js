1db4faa35e9de02db3cec247f019a3042d9eefff
iamjoel <iamjoel007@gmail.com>
2022-02-21 12:05:02 +0800

    two-indicator

--- src/array/06-two-sum/two-indicator.js
+++ src/array/06-two-sum/two-indicator.js
@@ -1,6 +1,20 @@
 /**
- * 双指针来实现。左指向头，右指向尾。大于目标则右指针右移；否则左移。
-*/
-const twoSum = function(numbers, target) {
+ * 双指针来实现。左指向头，右指向尾。大于目标则右指针左移；否则左指针右移。
+ */
+const twoSum = function (numbers, target) {
+  let leftIndex = 0;
+  let rightIndex = numbers.length - 1;
 
+  for (let i = 0; i < numbers.length; i++) {
+    let left = numbers[leftIndex];
+    let right = numbers[rightIndex];
+    if (left + right === target) {
+      return [leftIndex, rightIndex];
+    }
+    if (left + right > target) {
+      rightIndex--;
+    } else {
+      leftIndex++;
+    }
+  }
 };
