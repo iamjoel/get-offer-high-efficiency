51a89fe973a0872e58591450ad4759923c9d9aa1
iamjoel <iamjoel007@gmail.com>
2022-02-21 12:17:46 +0800

    dichotomy

--- src/array/06-two-sum/dichotomy.js
+++ src/array/06-two-sum/dichotomy.js
@@ -1,6 +1,23 @@
 /**
  * 求和用二分法来实现
+ * 会超出时间限制
  */
-const twoSum = function(numbers, target) {
+const twoSum = function (numbers, target) {
+  for (let leftIndex = 0; leftIndex < numbers.length; leftIndex++) {
+    let rightIndex = numbers.length - 1;
 
+    while (leftIndex < rightIndex) {
+      let left = numbers[leftIndex];
+      let right = numbers[rightIndex];
+
+      if (left + right === target) {
+        return [leftIndex, rightIndex];
+      }
+      if (left + right > target) {
+        rightIndex = Math.ceil((leftIndex + rightIndex) / 2);
+      } else {
+        break;
+      }
+    }
+  }
 };
