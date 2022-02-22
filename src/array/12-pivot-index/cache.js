e97fa6ede27cf1cd148d70f1a344984cadd46acd
金伟强 <jinweiqiang@coding.net>
2022-02-22 16:13:03 +0800

    fin 12

--- src/array/12-pivot-index/cache.js
+++ src/array/12-pivot-index/cache.js
@@ -1,15 +1,15 @@
 /**
  * 缓存值
  */
-const pivotIndex = function (nums) {
-  const sumCache = {}; // 前 n 个，不包含 n
+ const pivotIndex = function (nums) {
+  const sumCache = []; // 前 n 个，不包含 n
   let sumTotal = 0;
-  nums.forEach((num, i) => {
-    sumCache[i] = sumTotal;
+  nums.forEach((num) => {
+    sumCache.push(sumTotal);
     sumTotal += num;
   });
 
-  for (let i = 0; i < nums.length - 2; i++) {
+  for (let i = 0; i <= nums.length - 2; i++) {
     if (sumCache[i] === sumTotal - sumCache[i + 1]) {
       return i;
     }
