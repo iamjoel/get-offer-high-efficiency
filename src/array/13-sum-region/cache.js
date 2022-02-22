8fa071954fa5be96de0354db7d49d7c17759f0e9
iamjoel <iamjoel007@gmail.com>
2022-02-22 23:32:17 +0800

    13 temp

--- src/array/13-sum-region/cache.js
+++ src/array/13-sum-region/cache.js
@@ -10,13 +10,13 @@ const NumMatrix = function (matrix) {
       continue;
     }
     let rowSum = 0;
-    for (let col = 0; col < matrix[0].length; row++) {
+    for (let col = 0; col < matrix[0].length; col++) {
       if (col === 0) {
         sumCache[row][0] = matrix[row][0];
-        rowSum += matrix[row][col];
         continue;
       }
-      sumCache[row][col] = sumCache[row][col - 1] + rowSum;
+      rowSum += matrix[row][col];
+      sumCache[row][col] = sumCache[row - 1][col] + rowSum;
     }
   }
   this.sumCache = sumCache;
@@ -34,3 +34,8 @@ NumMatrix.prototype.sumRegion = function (row1, col1, row2, col2) {
     (row1 > 0 && col1 > 0 ? sumCache[row1][col1] : 0)
   );
 };
+
+// ["NumMatrix","sumRegion","sumRegion","sumRegion"]
+// [[[[3,0,1,4,2],[5,6,3,2,1],[1,2,0,1,5],[4,1,0,1,7],[1,0,3,0,5]]],[2,1,4,3],[1,1,2,2],[1,2,2,4]]
+//
+// 预期 [null,8,11,12]
