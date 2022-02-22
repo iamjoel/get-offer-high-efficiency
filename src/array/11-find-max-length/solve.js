a4ac3254b5180785534b55e223d73f39f7966b34
金伟强 <jinweiqiang@coding.net>
2022-02-22 12:39:46 +0800

    just add

---
+++ src/array/11-find-max-length/solve.js
@@ -0,0 +1,7 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const findMaxLength = function(nums) {
+
+};
---
+++ src/array/12-pivot-index/cache.js
@@ -0,0 +1,7 @@
+/**
+ * 左侧的值 = 和处以2
+ * 把前n个数的下标都存起来。 sumCache[n] = sumTotal / 2。 结果是 n + 1
+ */
+const pivotIndex = function(nums) {
+
+};
