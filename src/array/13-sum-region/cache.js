7f00cfde7e22fd5f9153dd3052aa9e1e25601dd6
金伟强 <jinweiqiang@coding.net>
2022-02-23 13:59:30 +0800

    fix 13

--- src/array/13-sum-region/cache.js
+++ src/array/13-sum-region/cache.js
@@ -6,15 +6,15 @@ const NumMatrix = function (matrix) {
   for (let row = 0; row < matrix.length; row++) {
     sumCache[row] = [];
     if (row === 0) {
-      sumCache[0] = [...matrix[0]];
+      let rowSum = 0;
+      for (let col = 0; col < matrix[0].length; col++) {
+        rowSum += matrix[0][col]
+        sumCache[0][col] = rowSum;
+      }
       continue;
     }
     let rowSum = 0;
     for (let col = 0; col < matrix[0].length; col++) {
-      if (col === 0) {
-        sumCache[row][0] = matrix[row][0];
-        continue;
-      }
       rowSum += matrix[row][col];
       sumCache[row][col] = sumCache[row - 1][col] + rowSum;
     }
@@ -28,14 +28,19 @@ const NumMatrix = function (matrix) {
 NumMatrix.prototype.sumRegion = function (row1, col1, row2, col2) {
   const sumCache = this.sumCache;
   return (
-    sumCache[row1][col1] -
+    sumCache[row2][col2] -
     (row1 > 0 ? sumCache[row1 - 1][col2] : 0) -
     (col1 > 0 ? sumCache[row2][col1 - 1] : 0) +
-    (row1 > 0 && col1 > 0 ? sumCache[row1][col1] : 0)
+    (row1 > 0 && col1 > 0 ? sumCache[row1 - 1][col1 - 1] : 0)
   );
 };
 
-// ["NumMatrix","sumRegion","sumRegion","sumRegion"]
-// [[[[3,0,1,4,2],[5,6,3,2,1],[1,2,0,1,5],[4,1,0,1,7],[1,0,3,0,5]]],[2,1,4,3],[1,1,2,2],[1,2,2,4]]
-//
-// 预期 [null,8,11,12]
+
+// const matrix = new NumMatrix([
+//   [3,0,1,4,2],
+//   [5,6,3,2,1],
+//   [1,2,0,1,5],
+//   [4,1,0,1,7],
+//   [1,0,3,0,5]
+// ])
+// console.log(matrix.sumRegion(2,1,4,3)) 8
