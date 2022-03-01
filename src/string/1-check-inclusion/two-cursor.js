6ec96b3f970c2e0cc880b1b4a06260c735cfec9a
iamjoel <iamjoel007@gmail.com>
2022-03-01 15:27:09 +0800

    fin 14

--- src/string/1-check-inclusion/two-cursor.js
+++ src/string/1-check-inclusion/two-cursor.js
@@ -1,6 +1,3 @@
-/**
- * 根据变换词的特点来
- */
 const checkInclusion = function (s1, s2) {
   if (s1.length > s2.length) {
     return false;
@@ -15,13 +12,11 @@ const checkInclusion = function (s1, s2) {
     return true;
   }
 
-  let start = 1;
+  let start = 0;
   let end = start + s1.length;
   for (; end < s2.length; start++, end++) {
-    const removeIndex = start - 1;
-    count[s2[removeIndex]] = (count[s2[removeIndex]] || 0) - 1; // 出
-    const addIndex = end;
-    count[s2[addIndex]] = (count[s2[addIndex]] || 0) + 1; // 入
+    count[s2[start]] = (count[s2[start]] || 0) + 1; // 出
+    count[s2[end]] = (count[s2[end]] || 0) - 1; // 入
     if (isAllZero(count)) {
       return true;
     }
