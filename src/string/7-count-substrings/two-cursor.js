ca76c1f4a07e0c4dc24c988b1ed8e1f83c21a9cc
iamjoel <iamjoel007@gmail.com>
2022-03-02 13:53:46 +0800

    20 fin

--- src/string/7-count-substrings/two-cursor.js
+++ src/string/7-count-substrings/two-cursor.js
@@ -1,9 +1,9 @@
 /**
  * 从中间往两边，回文是奇数和偶数的情况。
  */
-const countSubstrings = function (s) {
+ const countSubstrings = function (s) {
   let count = 0;
-  for (let i = 0; i < s; i++) {
+  for (let i = 0; i < s.length; i++) {
     count += findPalindromeByCenter(s, i, i);
     count += findPalindromeByCenter(s, i, i + 1);
   }
@@ -13,7 +13,7 @@ const countSubstrings = function (s) {
 // 偶数是 c2 = c1 + 1; 奇数时 c1 = c2
 function findPalindromeByCenter(s, c1, c2) {
   let count = 0;
-  while (c1 > 0 && c2 < s.length) {
+  while (c1 >= 0 && c2 < s.length) {
     if (isPalindrome(s.slice(c1, c2 + 1))) {
       count += 1;
     }
