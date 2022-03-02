cecc7906068c2b3d0b567f3f6a8c17972338a9a5
iamjoel <iamjoel007@gmail.com>
2022-03-02 10:26:13 +0800

    18 temp

---
+++ src/string/5-is-palindrome/two-cursor.js
@@ -0,0 +1,21 @@
+/**
+ * 两边向中间遍历
+ */
+const isPalindrome = function (s) {
+  const letterNumberStr = s
+    .split('')
+    .filter((c) => /0-9a-zA-Z/.test(c))
+    .join('');
+  const formatedStr = letterNumberStr.toLowerCase();
+
+  const len = formatedStr.length;
+  const isEvenLen = len % 2 === 0;
+  const middleEndIndex = isEvenLen ? len / 2 - 1 : Math.floor(len / 2);
+
+  for (let i = 0; i <= middleEndIndex; i++) {
+    if (formatedStr[i] !== formatedStr[len - i - 1]) {
+      return false;
+    }
+  }
+  return true;
+};
