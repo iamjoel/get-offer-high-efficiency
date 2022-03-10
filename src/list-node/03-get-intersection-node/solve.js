97b6c2c6fbe623cdb445e34eb44c2ce98fc12cdb
iamjoel <iamjoel007@gmail.com>
2022-03-10 16:27:18 +0800

    23 finish

--- src/list-node/03-get-intersection-node/solve.js
+++ src/list-node/03-get-intersection-node/solve.js
@@ -1,38 +1,40 @@
 /*
-* 暴力破解。 链表A中，每遍历一个，都去链表B中找。
-* 另外一个方案，将链表A的尾部，连到链表B的头部，问题就转换成了找新的链表C的环的入口的问题。改变会链表会报错，如何去复制一个链表。
-* 遍历下A和B，存起来。从后往前遍历。
-*/
+ * 暴力破解。 链表A中，每遍历一个，都去链表B中找。
+ * 另外一个方案，将链表A的尾部，连到链表B的头部，问题就转换成了找新的链表C的环的入口的问题。改变会链表会报错，如何去复制一个链表。
+ * 遍历下A和B，存起来。从后往前遍历。
+ */
 const getIntersectionNode = function (headA, headB) {
-    if (!headA || !headB) return null;
-    const { stack: stackA, length: lengthA } = toStack(headA);
-    const { stack: stackB, length: lengthB } = toStack(headB);
-    if (stackA[lengthA - 1] !== stackB[lengthB - 1]) {
-      return null;
-    }
-  
-    const minLength = Math.min(lengthA, lengthB);
-    for (let i = 1; i <= minLength; i++) {
-      if (stackA[lengthA - i] !== stackB[lengthB - i]) {
-        const diff = stackA[lengthA - i];
-        return diff.next;
-      }
-    }
-  };
-  
-  function toStack(head) {
-    const stack = [];
-    let length = 0;
-    while (head) {
-      stack.push(head);
-      head = head.next;
-      length++;
-    }
-    return {
-      stack,
-      length,
-    };
+  if (!headA || !headB) return null;
+  const { stack: stackA, length: lengthA } = toStack(headA);
+  const { stack: stackB, length: lengthB } = toStack(headB);
+  if (stackA[lengthA - 1] !== stackB[lengthB - 1]) {
+    return null;
   }
+
+  const minLength = Math.min(lengthA, lengthB);
+  for (let i = 1; i <= minLength; i++) {
+    if (stackA[lengthA - i] !== stackB[lengthB - i]) {
+      const diff = stackA[lengthA - i];
+      return diff.next;
+    }
+  }
+  // 一个链表整个和另一个链表交叉。
+  return lengthA <= lengthB ? stackA[0] : stackB[0];
+};
+
+function toStack(head) {
+  const stack = [];
+  let length = 0;
+  while (head) {
+    stack.push(head);
+    head = head.next;
+    length++;
+  }
+  return {
+    stack,
+    length,
+  };
+}
 // const getIntersectionNode = function (headA, headB) {
 //     let indexA = headA;
 
@@ -42,7 +44,7 @@ const getIntersectionNode = function (headA, headB) {
 //     indexA.next = headB;
 //     return detectCycle(headA);
 //   };
-  
+
 // const detectCycle = (head) => {
 //     if (!head) {
 //       return null;
@@ -71,7 +73,7 @@ const getIntersectionNode = function (headA, headB) {
 //         break;
 //       }
 //     }
-  
+
 //     // 第三遍，快指针先走环的长度步，然后慢指针和快指针一起走。相遇的地方，就是环的入口。
 //     slowIndex = head;
 //     fastIndex = head;
@@ -84,4 +86,3 @@ const getIntersectionNode = function (headA, headB) {
 //     }
 //     return slowIndex;
 //   };
-  
