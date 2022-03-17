67067577d0f70ff1dc54924451727e4fe33db80c
金伟强 <jinweiqiang@coding.net>
2022-03-17 13:03:18 +0800

    aa

--- src/stack/2-asteroid-collision/solve.js
+++ src/stack/2-asteroid-collision/solve.js
@@ -1,61 +1,65 @@
 /**
+ * + 正右，负左。 右左 撞。其他都不撞。 往右边找到第一个右的。
  * 1. 符号相同的则入栈，不同则出栈，做碰撞。
  * 2. 碰撞的结果，再和第栈的第一个比较，
  *  2.1 如果是异号，则继续碰撞。循环第2步直到栈为空。
  *  2.2 如果是同号，则入栈。符号改成该符号。
  * 3. 往下找下一个
  */
- const asteroidCollision = function (asteroids) {
+const asteroidCollision = function (asteroids) {
   if (!asteroids) {
     return [];
   }
-  asteroids = asteroids.filter(item => item !== 0);
+  asteroids = asteroids.filter((item) => item !== 0);
   if (asteroids.length === 0) {
     return [];
   }
-  const stack = [asteroids[0]];
-  let currSign = asteroids[0] > 0 ? 1 : -1;
-  // debugger
-  for (let i = 1; i < asteroids.length; i++) {
+  const stack = [];
+  for (let i = 0; i < asteroids.length; i++) {
     const curr = asteroids[i];
-    if(stack.length === 0) {
-      currSign = curr > 0 ? 1 : -1
+    if (stack.length === 0) {
       stack.push(curr);
       continue;
     }
-    if (curr * currSign > 0) {
-      // 同号
-      stack.push(curr);
-    } else {
-      // 异号。碰撞
-      let isBigger = false;
-      while (stack.length > 0) {
-        const top = stack[0];
-        if (Math.abs(top) > Math.abs(curr)) {
-          // 栈里的都是同号的
-          isBigger = false;
-          break;
-        } else if (Math.abs(top) === Math.abs(curr)) {
-          // 同归于尽
-          isBigger = false;
-          stack.pop();
-          break;
-        } else {
-          // 继续碰撞
-          isBigger = true;
-          stack.pop();
-          continue;
-        }
-      }
-      // 把栈里的都撞光了。
-      if (isBigger && stack.length === 0) {
+
+    // 碰撞
+    let isBig = false;
+    while (stack.length > 0) {
+      // debugger
+      const last = stack[stack.length - 1];
+      // 不碰撞
+      if(!(last > 0 && curr < 0)) {
         stack.push(curr);
-        currSign = -currSign;
+        break;
       }
+      // last > 0 && curr < 0
+      if (last > -curr) {
+        isBig = false;
+        break;
+      } else if (last === -curr) {
+        // 同归于尽
+        isBig = false;
+        stack.pop();
+        break;
+      } else {
+        // 继续碰撞
+        stack.pop();
+        isBig = true;
+        continue;
+      }
+    }
+
+    if(stack.length === 0 && isBig) {
+      stack.push(curr);
     }
   }
   return stack;
 };
 
-asteroidCollision([5,10,-5])
+// console.log(asteroidCollision([5,10,-5]))
+// console.log(asteroidCollision([8,-8]))
+// console.log(asteroidCollision([10,2,-5]))
+// console.log(asteroidCollision([-2,-1,1,2]))
+// console.log(asteroidCollision([-2,-2,1,-2]))
+// console.log(asteroidCollision([1,-2,-2,-2]))
   
